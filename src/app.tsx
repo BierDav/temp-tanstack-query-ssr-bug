7da58b91f38a23aa6bb5fe133d9c3d330c7f670f
David Bieregger <david-e@outlook.de>
2024-06-06 22:52:26 +0200

    Add setTimout

--- src/app.tsx
+++ src/app.tsx
@@ -1,15 +1,22 @@
-import {MetaProvider, Title} from "@solidjs/meta";
-import {Router} from "@solidjs/router";
-import {FileRoutes} from "@solidjs/start/router";
-import {Suspense} from "solid-js";
-import {QueryClient, QueryClientProvider} from "@tanstack/solid-query";
-import {SolidQueryDevtools} from "@tanstack/solid-query-devtools";
+import { MetaProvider, Title } from "@solidjs/meta";
+import { Router } from "@solidjs/router";
+import { FileRoutes } from "@solidjs/start/router";
+import { Suspense } from "solid-js";
+import { QueryClient, QueryClientProvider } from "@tanstack/solid-query";
+import { SolidQueryDevtools } from "@tanstack/solid-query-devtools";
 
 export default function App() {
     return (
         <Router
             root={props => (
-                <QueryClientProvider client={new QueryClient()}>
+                <QueryClientProvider client={new QueryClient({
+                    defaultOptions: {
+                        queries: {
+                            refetchOnMount: false,
+                            suspense: true
+                        }
+                    }
+                })}>
                     <MetaProvider>
                         <Title>SolidStart - Basic</Title>
                         <Suspense>{props.children}</Suspense>
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,15 +1,19 @@
-import { ErrorBoundary } from "solid-js";
+import { ErrorBoundary, Suspense } from "solid-js";
 import { createQuery } from "@tanstack/solid-query";
 
 export default function () {
     const errorQuery = createQuery(() => ({
         queryKey: ["errorQuery"],
-        queryFn: () => {
-            throw new Error("test")
-        }
+        queryFn: () => new Promise<string>((_, reject) => {
+            setTimeout(() => {
+                reject(new Error("test"))
+            }, 1000)
+        })
     }))
 
-    return <ErrorBoundary fallback={<div>Something went wrong</div>}>
-        {errorQuery.data}
-    </ErrorBoundary>
+    return <Suspense>
+        <ErrorBoundary fallback={<div>Something went wrong</div>}>
+            {errorQuery.data}
+        </ErrorBoundary>
+    </Suspense>
 }
