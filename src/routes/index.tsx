10fa92d115f8fa149e4f936415b1da5b0adb342d
David Bieregger <david-e@outlook.de>
2024-06-06 22:29:26 +0200

    Reproduce hydration of error query

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,21 +1,15 @@
-import {createQuery} from "@tanstack/solid-query";
+import { ErrorBoundary } from "solid-js";
+import { createQuery } from "@tanstack/solid-query";
 
 export default function () {
-    const timeout = createQuery(() => ({
-        queryKey: ["timeout"],
-        queryFn: () => new Promise((resolve) => setTimeout(() => resolve("test"), 2000))
+    const errorQuery = createQuery(() => ({
+        queryKey: ["errorQuery"],
+        queryFn: () => {
+            throw new Error("test")
+        }
     }))
 
-    const state = createQuery(() => ({
-        queryKey: ["realQuery"],
-        queryFn: async () => {
-            const resp = await fetch('https://api.sampleapis.com/switch/games');
-            return await resp.json();
-        },
-        enabled: !!timeout.data
-    }))
-
-    return <>
-        {JSON.stringify(state.data?.[0])}
-    </>
+    return <ErrorBoundary fallback={<div>Something went wrong</div>}>
+        {errorQuery.data}
+    </ErrorBoundary>
 }
