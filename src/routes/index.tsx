e62e0bc0537d2eb3cd636529540f39a93ea60ef9
David Bieregger <david@bieregger.at>
2024-07-17 18:23:34 +0200

    Showcase the problem

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,15 +1,23 @@
-import { ErrorBoundary } from "solid-js";
-import { createQuery } from "@tanstack/solid-query";
+import { createQueries } from "@tanstack/solid-query";
+import { ErrorBoundary, Suspense } from "solid-js";
 
 export default function () {
-    const errorQuery = createQuery(() => ({
-        queryKey: ["errorQuery"],
-        queryFn: () => {
-            throw new Error("test")
-        }
-    }))
+    const queries = createQueries(() => {
+        const list = ["test1", "test2"];
+        return ({
+            queries: list.map((key) => ({
+                queryKey: ["key", key],
+                queryFn: () => true,
+            })),
+            combine: (results) => ({
+                data: results.every(result => result.data)
+            })
+        });
+    });
 
     return <ErrorBoundary fallback={<div>Something went wrong</div>}>
-        {errorQuery.data}
+        <Suspense>
+            {queries.data}
+        </Suspense>
     </ErrorBoundary>
 }
